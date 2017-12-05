c83305633cb61a401e50b5109d15872861cbd213
Leekane <lk1403994707@outlook.com>
2017-12-05 17:14:39 +0800

    前后分离

--- src/main/java/controller/UsersController.java
+++ src/main/java/controller/UsersController.java
@@ -4,17 +4,21 @@ package controller;
  * Created by mac on 16/7/16.
  */
 import bean.user;
+import net.sf.json.JSONObject;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import service.userService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @Controller
@@ -36,9 +40,20 @@ public class UsersController {
 
         return "index";
     }
-    @RequestMapping(value = "/picture")
-    public String picture() {
-
-        return "picture";
+    @ResponseBody
+    @RequestMapping(value = "/getAllJson",method = RequestMethod.GET)
+    public Map getAllJson(){
+        Map<String, Object> result = new HashMap<String, Object>();
+        result.put("json","sss");
+        return result;
     }
+
+    @ResponseBody
+    @RequestMapping(value = "/postUser",method = RequestMethod.POST)
+    public Map postUser (@RequestBody Map<String,String> requestUser ){
+        String userName = requestUser.get("userName");
+        System.out.println(userName);
+        return requestUser;
+    }
+
 }
